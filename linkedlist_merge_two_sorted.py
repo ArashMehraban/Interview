4c65f8920d2a702ff1ab7e1c310abd02ba4202b8
Arash <arashm81@gmail.com>
2021-09-09 17:54:51 -0700

    add two numbers in linked lists

---
+++ linkedlist_merge_two_sorted.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if not l1: 
+            return l2
+        if not l2:
+            return l1
+        
+        cur = dummy = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
