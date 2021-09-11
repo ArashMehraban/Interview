632ae8731b659d040a99e16e754810239d745bf5
Arash <arashm81@gmail.com>
2021-09-11 11:21:04 -0700

    find the length of longest substring without repeats

--- arr_longest_substr_len.py
+++ arr_longest_substr_len.py
@@ -1,17 +1,14 @@
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        cur, sofar,start = 0,0,0
-        N = len(s)
-        i = 0
-        lookup ={}
-        while i<N:
-            if s[i] not in lookup:
-                cur+=1
-            else:
-                start = max(start, lookup[s[i]])
-                cur = i - start
-            lookup[s[i]] = i
-            sofar = max(cur,sofar)
-            i+=1
-        return sofar
-        
+	def lengthOfLongestSubstring(self, s: str) -> int:
+		start = -1   # start index of current substring
+		longest = 0  # length of the longest substring
+		hash_map = dict() # hash map is to store the latest index of char
+
+		for i in range(len(s)):
+			if s[i] in hash_map and start < hash_map[s[i]]:
+                            start = hash_map[s[i]] 
+			hash_map[s[i]] = i 
+			if i - start > longest:
+                            longest = i - start 
+
+		return longest
