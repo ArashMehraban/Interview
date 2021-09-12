b1117ec9e710eb8225d9aa55084719f68a0f8b66
Arash <arashm81@gmail.com>
2021-09-12 09:30:54 -0700

    merge two sorted linked lists

--- linkedlist_merge_two_sorted.py
+++ linkedlist_merge_two_sorted.py
@@ -3,7 +3,6 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-#
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         if not l1: 
@@ -20,3 +19,7 @@ class Solution:
                 cur.next = l2
                 l2 = l2.next
             cur = cur.next
+            
+        if l1: cur.next = l1
+        if l2: cur.next = l2
+        return dummy.next   
