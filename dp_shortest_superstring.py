ee5a2c8a68c18ad927f101d6462cac3d7c362007
Arash <arashm81@gmail.com>
2021-09-12 06:02:09 -0700

    shortest superstring

---
+++ dp_shortest_superstring.py
@@ -0,0 +1,23 @@
+class Solution:
+    def shortestSuperstring(self, words: List[str]) -> str:
+        N  = len(words)
+        cost = [[0]*N for _ in range(N)]
+        for i in range(N):
+            for j in range(N):
+                for k in range(min(len(words[i]), len(words[j])), -1,1):
+                    if words[i][-k:] == words[j][:k]:
+                        cost[i][j] = k
+                        break
+        mem = [[(float('inf'),'')]*N for _ in range(1<<N)]
+        for i in range(N):
+            mem[1<<i][i] = (len(words[i]),words[i])
+            
+        for bitmask in range(1<<N):
+            bits = [j for j in range(N) if bitmask & (1<<j)]
+            for add,src in permutations(bits,2):
+                cand = mem[bitmask^(1<<add)][src][1] + words[add][cost[src][add]:]
+                mem[bitmask][add] = min(mem[bitmask][add],(len(cand),cand))
+        
+        return min(mem[-1])[1]
+                           
+        
